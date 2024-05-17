930988f7d41ab381f24bb4472ad4b90e0cc14ec9
dgreen42 <123395249+dgreen42@users.noreply.github.com>
2024-05-16 21:00:28 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ where
     fasta
 }
 
-fn read_fasta<P>(filename: P) -> HashMap<String, String>
+fn read_multiple_fasta_deco<P>(filename: P) -> HashMap<String, String>
 where
     P: AsRef<Path>,
 {
@@ -36,6 +36,34 @@ where
     let file = File::open(filename).expect("Could not open file");
     let gz = MultiGzDecoder::new(file);
     let buf = BufReader::new(gz);
+    let mut fasta = HashMap::new();
+    let mut curid = String::new();
+    let mut curseq = String::new();
+    for line in buf.lines() {
+        let line = line.expect("Could not read line");
+        if line.starts_with('>') {
+            if !curid.is_empty() {
+                fasta.insert(curid.clone(), curseq.clone());
+                curseq.clear();
+            }
+            // println!("{:?}", &line[..].trim());
+            curid = line[..].trim().to_string();
+        } else {
+            curseq.push_str(line.trim());
+        }
+    }
+    let duration = start.elapsed();
+    println!("It took {:?} to decode and read", duration);
+    fasta
+}
+
+fn read_multiple_fasta<P>(filename: P) -> HashMap<String, String>
+where
+    P: AsRef<Path>,
+{
+    let start = Instant::now();
+    let file = File::open(filename).expect("Could not open file");
+    let buf = BufReader::new(file);
     let mut fasta = HashMap::new();
     let mut curid = String::new();
     let mut curseq = String::new();
